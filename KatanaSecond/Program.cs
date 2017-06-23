2e231155f4df5869fa8d3b480e02743a7ea5bed1
Timmy Tom <timmy.tom@cba.com.au>
2017-06-23 16:37:21 +1000

    Simple Pipeline Example

--- KatanaSecond/Program.cs
+++ KatanaSecond/Program.cs
@@ -30,7 +30,41 @@ namespace KatanaSecond
     {
         public void Configuration(IAppBuilder app)
         {
-            //app.Use<HelloWorldComponent>();
+            // Pipe line 1
+            app.Use(async (context, next) =>
+            {
+                // can add a new key and pass to others
+                context.Environment.Add("myPipeline", "pipleline 1");
+                foreach (var pair in context.Environment)
+                {
+                    Console.WriteLine("{0}:{1}", pair.Key, pair.Value);
+                }
+                Console.WriteLine("*******************************");
+                await next();
+            });
+
+            // make HelloWorldComponent invoke method async and put await code
+            //app.UseHelloWorld();
+
+            // Pipe line 2
+            app.Use(async (context, next) =>
+            {
+                context.Environment["myPipeline"] = "pipleline 2";
+                var response = context.Environment["owin.ResponseBody"] as Stream;
+                using (var writer = new StreamWriter(response))
+                {
+                    writer.WriteLine("Helloo I am from another middle ware!");
+                }
+                Console.WriteLine("Pipe Line in second middleware: {0} ", context.Environment["myPipeline"]);
+                Console.WriteLine("*******************************");
+                Console.WriteLine("Requesting : {0}", context.Environment["owin.RequestPath"]);
+                await next();
+                Console.WriteLine("Response : {0}", context.Environment["owin.ResponseStatusCode"]);
+                Console.WriteLine("Pipe Line in second middleware End: {0} ", context.Environment["myPipeline"]);
+                Console.WriteLine("*******************************");
+            });
+
+            // Pipe line 3
             app.UseHelloWorld();
         }
     }
@@ -47,19 +81,24 @@ namespace KatanaSecond
     public class HelloWorldComponent
     {
         AppFunc _next;
-        public HelloWorldComponent(Func<IDictionary<string, object>, Task> next)
-        //public HelloWorldComponent(AppFunc next)
+        public HelloWorldComponent(AppFunc next)
         {
             _next = next;
         }
 
-        public Task Invoke(IDictionary<string, object> environment)
+        public Task Invoke(IDictionary<string, object> context)
+        //public async Task Invoke(IDictionary<string, object> context)
         {
-            var response = environment["owin.ResponseBody"] as Stream;
+            context["myPipeline"] = "pipleline 3";
+            Console.WriteLine("Pipe Line in third middleware: {0}", context["myPipeline"]);
+            Console.WriteLine("*******************************");
+            var response = context["owin.ResponseBody"] as Stream;
             using (var writer = new StreamWriter(response))
             {
+                // writer.WriteLine("Helloo I am from Hello Component!");
                 return writer.WriteAsync("Helloo I am from Hello Component!");
             }
+            // await _next(context);
         }
     }
 }
